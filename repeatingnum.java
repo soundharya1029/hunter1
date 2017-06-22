7a91ce09cdfc7a84256bbd40441f03483626ff4a
soundharya1029 <mvsoundharya97@gmail.com>
2017-06-22 11:58:30 +0530

    Create repeatingnum.java

---
+++ repeatingnum.java
@@ -0,0 +1,38 @@
+public class repeatingnum {
+
+	public static void main(String[] args)
+	{
+     int c=0;
+     int ch[]=new int[args.length];
+     int ch1[]=new int[args.length];
+     for(int i=0;i<args.length;i++)
+     {
+    	 ch[i]=Integer.parseInt(args[i]);
+     }
+     for(int j=0;j<args.length;j++)
+     {
+    	 if(j<args.length-1)
+    	 {
+    	 for(int k=j+1;k<args.length;k++)
+    	 {
+    		if(ch[j]==ch[k])
+    		{
+    			for(int s=0;s<ch.length;s++)
+    			{
+    				if(ch1[s]!=ch[j])
+    				{
+    			ch1[c]=ch[k];
+    			
+    		}
+    			}
+    			c++;
+    		}
+    	 }
+    	 }
+     }
+     for(int m=0;m<c;m++)
+     System.out.println(ch1[m]);
+     
+	}
+
+}
